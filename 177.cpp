28aa23b69e9f05cab3e89490837c4ea110bb7442
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-05-02 20:22:41 +0530

    Add files via upload

---
+++ 177.cpp
@@ -0,0 +1,54 @@
+// level order traversal using queue
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    node *left;
+    node *right;
+    int data;
+};
+node *createnode(int data)
+{
+    node *n = new node;
+
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+void levelordertraversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        node *n = q.front();
+        q.pop();
+        cout << n->data << " ";
+        if (n->left != NULL)
+        {
+            q.push(n->left);
+        }
+        if (n->right != NULL)
+        {
+            q.push(n->right);
+        }
+    }
+}
+
+int main()
+{
+    node *root = createnode(1);
+    node *a = createnode(2);
+    node *b = createnode(3);
+    node *c = createnode(4);
+    node *d = createnode(5);
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    levelordertraversal(root);
+    // cout << "root ka data he " << root->data;
+
+    return 0;
+}
