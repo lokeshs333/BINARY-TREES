e02388ec26a111669fbe7aa8e9e940f02d7eff33
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-05-04 01:17:19 +0530

    Add files via upload

---
+++ 182.cpp
@@ -0,0 +1,49 @@
+// inorder of a tree
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
+node *createnode(int data)
+{
+    node *n = new node;
+    n->data = data;
+    n->right = NULL;
+    n->left = NULL;
+    return n;
+}
+void inorder(node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main()
+{
+    node *root = createnode(1);
+    node *a = createnode(2);
+    node *b = createnode(3);
+    node *c = createnode(4);
+    node *d = createnode(5);
+    node *e = createnode(6);
+    node *f = createnode(7);
+    node *g = createnode(8);
+
+    root->left = a;
+    a->left = c;
+    root->right = b;
+    b->left = d;
+    b->right = e;
+    d->left = f;
+    d->right = g;
+    inorder(root);
+    return 0;
+}
