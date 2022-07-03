877b1dbcefab36099b6f42c4e134697aa8fa2fef
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-07-03 18:24:50 +0530

    Add files via upload

---
+++ 204.cpp
@@ -0,0 +1,75 @@
+// sum of maximum subtree in an tree.....
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
+node *createnode(int data)
+{
+    node *n = new node;
+    n->data = data;
+    n->right = NULL;
+    n->left = NULL;
+    return n;
+}
+int m = 0;
+int max_sum(node *head)
+{
+    if (!head)
+    {
+        return 0;
+    }
+    int l = max_sum(head->left);
+    int r = max_sum(head->right);
+    m = max(m, l + r + head->data);
+    return l + r + head->data;
+}
+void inorder(node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main()
+{
+    node *root = createnode(1);
+    node *a = createnode(2);
+    node *b = createnode(3);
+    node *c = createnode(4);
+    node *d = createnode(5);
+    node *e = createnode(6);
+    node *f = createnode(7);
+    node *g = createnode(8);
+    /*              1
+                   /  \
+                 2     3
+                /     / \
+              4      5   6
+                    / \
+                  7     8      */
+    root->left = a;
+    a->left = c;
+    root->right = b;
+    b->left = d;
+    b->right = e;
+    d->left = f;
+    d->right = g;
+    cout << endl
+         << "hello lokesh pandey" << endl;
+
+    cout << "lokesh pandey" << endl;
+    int aa = max_sum(root);
+    cout << "answer is " << aa << endl;
+
+    return 0;
+}
