8d23437026b9e4278ea5c07d194decf9e060ede9
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-07-04 14:45:22 +0530

    Add files via upload

---
+++ 208.cpp
@@ -0,0 +1,109 @@
+// distance between two nodes..........
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
+node *createnode(int data)
+{
+    node *n = new node;
+    n->data = data;
+    n->right = NULL;
+    n->left = NULL;
+    return n;
+}
+
+void inorder(node *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+node *lca(node *root, node *p, node *q)
+{
+    if (root == nullptr || root == p || root == q)
+    {
+        return root;
+    }
+    node *left = lca(root->left, p, q);
+    node *right = lca(root->right, p, q);
+    if (left == nullptr)
+    {
+        return right;
+    }
+    if (right == nullptr)
+    {
+        return left;
+    }
+    else if (left != nullptr && right != nullptr)
+    {
+        return root;
+    }
+}
+int solve(node *root, node *val)
+{
+    if (!root)
+    {
+        return 0;
+    }
+    if (root->data == val->data)
+    {
+        return 1;
+    }
+    int a = solve(root->left, val);
+    int b = solve(root->right, val);
+    if (!a && !b)
+    {
+        return 0;
+    }
+    return a + b + 1;
+}
+int find_distance(node *root, node *a, node *b)
+{
+    node *lcaa = lca(root, a, b);
+    int x = solve(lcaa, a);
+    int y = solve(lcaa, b);
+    return x + y - 2;
+}
+int main()
+{
+    node *root = createnode(1);
+    node *a = createnode(2);
+    node *b = createnode(3);
+    node *c = createnode(4);
+    node *d = createnode(5);
+    node *e = createnode(6);
+    node *f = createnode(7);
+    node *g = createnode(8);
+    /*              1
+                   /  \
+                 2     3
+                /     / \
+              4      5   6
+                    / \
+                  7     8      */
+    root->left = a;
+    a->left = c;
+    root->right = b;
+    b->left = d;
+    b->right = e;
+    d->left = f;
+    d->right = g;
+    cout << endl
+         << "hello lokesh pandey" << endl;
+
+    cout << "lokesh pandey" << endl;
+
+    int aaa = find_distance(root, c, f);
+    cout << "your answer is " << aaa << endl;
+
+    return 0;
+}
